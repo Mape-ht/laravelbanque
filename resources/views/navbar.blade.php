6d0fef82c87cc965869208a12acc39236193b66c
Mape-ht <hantamp@gmail.com>
2020-10-11 14:40:36 +0000

    list client physique et compte OK

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -6,7 +6,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <a class="nav-link" href="{{url('/')}}">Accueil <span class="sr-only">(current)</span></a>
         <a class="nav-link" href="{{url('/create')}}">Créer Client Entreprise </a>
-        <a class="nav-link" href="{{url('/clientphysique/createClientphysique')}}">Créer Client Particulier </a>
-        <a class="nav-link" href="{{url('/compte/createCompte')}}">Créer Compte </a>
+        <a class="nav-link" href="{{url('/createClientphysique')}}">Créer Client Particulier </a>
+        <a class="nav-link" href="{{url('/createCompte')}}">Créer Compte </a>
     </div>
 </nav>
